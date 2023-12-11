6267a187686bf1b1bf19dec28b87134ac03d64bc
Hilary Liu <2788370451@qq.com>
2023-12-11 11:01:36 +0800

    feat: add tooltip

--- packages/editor/src/extensions/components/CommonToolBar.tsx
+++ packages/editor/src/extensions/components/CommonToolBar.tsx
@@ -1,8 +1,41 @@
+import { useState } from 'react';
 import { BubbleItemProps } from '../../types';
-
+import {
+  FloatingPortal,
+  autoUpdate,
+  useDismiss,
+  useFloating,
+  useFocus,
+  useHover,
+  useInteractions,
+  useRole,
+} from '@floating-ui/react';
 const CommonToolBar = (props: BubbleItemProps) => {
   const { title, action, isActive, Icon } = props;
+  const [isOpen, setIsOpen] = useState(false);
 
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    placement: 'top',
+    // Make sure the tooltip stays on the screen
+    whileElementsMounted: autoUpdate,
+    middleware: [],
+  });
+  // Event listeners to change the open state
+  const hover = useHover(context, { move: false });
+  const focus = useFocus(context);
+  const dismiss = useDismiss(context);
+  // Role props for screen readers
+  const role = useRole(context, { role: 'tooltip' });
+
+  // Merge all the interactions into prop getters
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    hover,
+    focus,
+    dismiss,
+    role,
+  ]);
   return (
     <>
       <span
@@ -11,7 +44,21 @@ const CommonToolBar = (props: BubbleItemProps) => {
         }}
         className={`${isActive!() ? 'is-active' : ''} mx-1`}
       >
-        {Icon ? <Icon /> : title}
+        <FloatingPortal>
+          {isOpen && (
+            <div
+              className="Tooltip"
+              ref={refs.setFloating}
+              style={floatingStyles}
+              {...getFloatingProps()}
+            >
+              {title}
+            </div>
+          )}
+        </FloatingPortal>
+        <span ref={refs.setReference} {...getReferenceProps()}>
+          {Icon ? <Icon /> : title}
+        </span>
       </span>
     </>
   );
